922c3891abed805af8aaf1e7f4b43276f680d976
LordMungi <lrdmungi@gmail.com>
2025-10-28 23:32:23 -0300

    Minor fixes.

--- src/hud.cpp
+++ src/hud.cpp
@@ -11,17 +11,17 @@ namespace hud
 		{
 		case game::State::Playing:
 		{
-			render::text(std::to_string(gameStats.lives), { 5, 5 }, 5, RED);
+			render::text("Lives " + std::to_string(gameStats.lives), {10, 5}, 5, BLACK);
 			break;
 		}
 		case game::State::Won:
 		{
-			render::text("Winner!", {config::gamespace.x / 2, config::gamespace.y / 2 }, 10, RED);
+			render::text("Winner!", {config::gamespace.x / 2, config::gamespace.y / 2 }, 10, BLACK);
 			break;
 		}
 		case game::State::Lost:
 		{
-			render::text("Game Over", { config::gamespace.x / 2, config::gamespace.y / 2 }, 10, RED);
+			render::text("Game Over", { config::gamespace.x / 2, config::gamespace.y / 2 }, 10, BLACK);
 			break;
 		}
 
